81255227ca3a35575b816c28cddff775538eeb44
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-08 19:58:09 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,16 @@
 require 'pry'
 
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    # @@artists << self
+    super
     @songs = []
   end
 
